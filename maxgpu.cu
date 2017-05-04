f6c0ae81bb74a0af8d04edb08f6690cf331adec0
Muhammad Joyo <muhammadjoyo@172-17-15-168.DYNAPOOL.NYU.EDU>
2017-05-04 15:14:15 -0400

    df

--- maxgpu.cu
+++ maxgpu.cu
@@ -3,7 +3,39 @@
 #include <time.h>
 #include <cuda.h>
 
-unsigned int getmax(unsigned int *, unsigned int);
+__device__ unsigned int myMax(unsigned int* address, unsigned int val)
+{
+	unsigned int old = *address;
+	unsigned int assumed;
+	while (val > old) {
+		assumed = old;
+		old = atomicCAS(address, assumed, val);
+	}
+	return old;
+}
+
+__global__ void getmaxcu(unsigned int* numbers_d, unsigned int* max_d, int n) {
+
+	extern __shared__ unsigned int shared[];
+
+	int tid = threadIdx.x;
+	int gid = (blockDim.x * blockIdx.x) + tid;
+	shared[tid] = 0;
+
+	if (gid < n)
+		shared[tid] = numbers_d[gid];
+	__syncthreads();
+
+	for (unsigned int s = blockDim.x / 2; s > 0; s = s / 2) 
+	{
+		if (tid < s && gid < n)
+			shared[tid] = max(shared[tid], shared[tid + s]);
+		__syncthreads();
+	}
+
+	if (tid == 0)
+		max_d[0] = myMax(max_d, shared[0]);
+}
 
 int main(int argc, char *argv[]) {
 	unsigned int size = 0;  // The size of the array
@@ -50,37 +82,4 @@ int main(int argc, char *argv[]) {
 	cudaFree(max_d);
 	free(numbers);
 	exit(0);
-}
-
-__device__ unsigned int atomicMax(unsigned int* address, unsigned int val)
-{
-	unsigned int old = *address, assumed;
-	while (val > old) {
-		assumed = old;
-		old = atomicCAS(address_as_int, assumed, val);
-	}
-	return old;
-}
-
-__global__ void getmaxcu(unsigned int* numbers_d, unsigned int* max_d, int n) {
-
-	extern __shared__ unsigned int shared[];
-
-	int tid = threadIdx.x;
-	int gid = (blockDim.x * blockIdx.x) + tid;
-	shared[tid] = 0;
-
-	if (gid < n)
-		shared[tid] = numbers_d[gid];
-	__syncthreads();
-
-	for (unsigned int s = blockDim.x / 2; s > 0; s = s / 2) 
-	{
-		if (tid < s && gid < n)
-			shared[tid] = max(shared[tid], shared[tid + s]);
-		__syncthreads();
-	}
-
-	if (tid == 0)
-		atomicMax(max_d, shared[0]);
 }
