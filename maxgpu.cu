711e93d43a7d3a69c1c63e7d04236a82c6728042
Muhammad Joyo <muhammadjoyo@172-17-15-168.DYNAPOOL.NYU.EDU>
2017-05-04 20:29:31 -0400

    df

--- maxgpu.cu
+++ maxgpu.cu
@@ -99,12 +99,12 @@ int main(int argc, char *argv[]) {
 		cudaMemcpy(numbers_d, numbers, i * sizeof(unsigned int), cudaMemcpyHostToDevice);
 		getmaxcu<<<(int)ceil((float)i / TPB),TPB>>>(numbers_d, max_d, i);
 		i = (int)ceil((float)i / TPB);
-		printArr(max, size/TPB);
+		printArr(max, size/TPB + 1);
 		cudaMemcpy(max, max_d, i * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 		if(i == 1) {
 			done = 1;
 		}
-		printArr(max, size/TPB);
+		printArr(max, size/TPB + 1);
 
 	}
 
